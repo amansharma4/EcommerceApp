2e5036368c103a0e0c97330b80ad86afd40d4ab6
amansharma4 <amans7990@gmail.com>
2021-01-05 22:13:50 +0530

    addNavbarComponent

---
+++ src/components/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Col,
+  Image,
+} from "react-bootstrap";
+const NavComp = () => {
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Ecomm App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Image
+              src="https://p.kindpng.com/picc/s/30-303412_dark-white-circle-transparent-hd-png-download.png"
+              width="30px" roundedCircle
+            />
+            <NavDropdown title="" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">My Cart</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">My orders</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Settings</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavComp;
